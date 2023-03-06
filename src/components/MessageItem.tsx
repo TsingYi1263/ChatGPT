146d6fb8ec93616db1053510e000854b09b214ff
TsingYi1220 <987549601@qq.com>
2023-03-06 12:23:45 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/MessageItem.tsx
@@ -0,0 +1,48 @@
+import type { Accessor } from 'solid-js'
+import type { ChatMessage } from '@/types'
+import MarkdownIt from 'markdown-it'
+// @ts-ignore
+import mdKatex from 'markdown-it-katex'
+import mdHighlight from 'markdown-it-highlightjs'
+import IconRefresh from './icons/Refresh'
+
+interface Props {
+  role: ChatMessage['role']
+  message: Accessor<string> | string
+  showRetry?: Accessor<boolean>
+  onRetry?: () => void
+}
+
+export default ({ role, message, showRetry, onRetry }: Props) => {
+  const roleClass = {
+    system: 'bg-gradient-to-r from-gray-300 via-gray-200 to-gray-300',
+    user: 'bg-gradient-to-r from-purple-400 to-yellow-400',
+    assistant: 'bg-gradient-to-r from-yellow-200 via-green-200 to-green-300',
+  }
+  const htmlString = () => {
+    const md = MarkdownIt().use(mdKatex).use(mdHighlight)
+
+    if (typeof message === 'function') {
+      return md.render(message())
+    } else if (typeof message === 'string') {
+      return md.render(message)
+    }
+    return ''
+  }
+  return (
+    <div class="py-2 -mx-4 px-4 transition-colors md:hover:bg-slate/3">
+      <div class="flex gap-3 rounded-lg" class:op-75={role === 'user'}>
+        <div class={`shrink-0 w-7 h-7 mt-4 rounded-full op-80 ${roleClass[role]}`}></div>
+        <div class="message prose text-slate break-words overflow-hidden" innerHTML={htmlString()} />
+      </div>
+      { showRetry?.() && onRetry && (
+        <div class="flex items-center justify-end px-3 mb-2">
+          <div onClick={onRetry} class="flex items-center gap-1 px-2 py-0.5 op-70 border border-slate text-slate rounded-md text-sm cursor-pointer hover:bg-slate/10">
+            <IconRefresh />
+            <span>Regenerate</span>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
---
+++ src/pages/api/generate.ts
@@ -0,0 +1,31 @@
+import type { APIRoute } from 'astro'
+import { generatePayload, parseOpenAIStream } from '@/utils/openAI'
+import { verifySignature } from '@/utils/auth'
+// #vercel-disable-blocks
+import { fetch, ProxyAgent } from 'undici'
+// #vercel-end
+
+const apiKey = import.meta.env.OPENAI_API_KEY
+const https_proxy = import.meta.env.HTTPS_PROXY
+
+export const post: APIRoute = async (context) => {
+  const body = await context.request.json()
+  const { sign, time, messages } = body
+  if (!messages) {
+    return new Response('No input text')
+  }
+  if (import.meta.env.PROD && !await verifySignature({ t: time, m: messages?.[messages.length - 1]?.content || '', }, sign)) {
+    return new Response('Invalid signature')
+  }
+  const initOptions = generatePayload(apiKey, messages)
+  // #vercel-disable-blocks
+  if (https_proxy) {
+    initOptions['dispatcher'] = new ProxyAgent(https_proxy)
+  }
+  // #vercel-end
+
+  // @ts-ignore
+  const response = await fetch('https://api.openai.com/v1/chat/completions', initOptions) as Response
+
+  return new Response(parseOpenAIStream(response))
+}
