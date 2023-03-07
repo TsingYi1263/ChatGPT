8d1a4e272c94724f9a7134d1c522663296fa5942
dyk <987549601@qq.com>
2023-03-07 17:15:39 +0800

    update

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -17,7 +17,7 @@ export default ({ role, message, showRetry, onRetry }: Props) => {
   const roleClass = {
     system: 'bg-gradient-to-r from-gray-300 via-gray-200 to-gray-300',
     user: 'bg-gradient-to-r from-purple-400 to-yellow-400',
-    assistant: `'bg-gradient-to-r from-yellow-200 via-green-200 to-green-300'style="background-image:url(https://chat.openai.com/favicon-32x32.png);background-position: center;"`
+    assistant: 'bg-gradient-to-r from-yellow-200 via-green-200 to-green-300'
   }
   const htmlString = () => {
     const md = MarkdownIt().use(mdKatex).use(mdHighlight)
@@ -32,7 +32,14 @@ export default ({ role, message, showRetry, onRetry }: Props) => {
   return (
     <div class="py-2 -mx-4 px-4 transition-colors md:hover:bg-slate/3">
       <div class="flex gap-3 rounded-lg" class:op-80={role === 'user'}>
-        <div class={`shrink-0 w-7 h-7 mt-4 rounded-full op-80 ${roleClass[role]}`}></div>
+        <div
+          style={`${
+            roleClass[role] === 'assistant'
+              ? 'background-image:url(https://chat.openai.com/favicon-32x32.png);background-position: center;'
+              : ''
+          }`}
+          class={`shrink-0 w-7 h-7 mt-4 rounded-full op-80 ${roleClass[role]}`}
+        ></div>
         <div class="message prose text-slate break-words overflow-hidden" innerHTML={htmlString()} />
       </div>
       {showRetry?.() && onRetry && (
